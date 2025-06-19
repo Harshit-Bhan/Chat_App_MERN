881b8914d80f9022efa95bd21931bba47868ef88
Harshit-Bhan <harshitbhan2004@gmail.com>
2025-06-20 03:25:52 +0530

    MongoDb connected

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const connectDB = require('./config/db');
+const colors = require('colors')
+
 
 const app = express();
 // app.use(cors());
 dotenv.config();
+connectDB();
 
 app.get('/api',(req,res) => {
     res.send("API is Running");
@@ -12,4 +16,4 @@ app.get('/api',(req,res) => {
 
 const PORT = process.env.PORT || 5002;
 
-app.listen(PORT,console.log(`Server Started on Port ${PORT}`));
+app.listen(PORT,console.log(`Server Started on Port ${PORT}`.blue.bold));
